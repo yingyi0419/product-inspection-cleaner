1538b11e54ed9b50a63cbff41aef3e88eb815283
yingyi0419 <yingyi0419@gmail.com>
2026-07-10 23:34:30 +0800

    Update import path for App component

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./src/App.jsx";
 import "./App.css";
 
 createRoot(document.getElementById("root")).render(<App />);
