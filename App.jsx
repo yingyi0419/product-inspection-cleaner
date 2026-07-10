9b87194b9d0112a18ba49e54dd95123fa7389714
yingyi0419 <yingyi0419@gmail.com>
2026-07-10 23:35:37 +0800

    Update CSS import path in App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./src/App.jsx";
-import "./App.css";
+import "./src/App.css";
 
 createRoot(document.getElementById("root")).render(<App />);
